81286d11c9f3434d71570e8286f1aefcd54a2b9d
fdur <chantmforfun@gmail.com>
2019-07-17 12:49:55 +0800

    add average

--- read.py
+++ read.py
@@ -4,8 +4,12 @@ with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 #count = count + 1
-		if count % 1000 == 0:
-		    print(len(data))# print use time 
-print(len(data))
-print('-------------')
-print(data[2])
+		#if count % 1000 == 0:
+		    #print(len(data))# 'print' use time 
+#print('檔案讀取完，總共有',len(data),'筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	
+print('字元的平均長度是', sum_len / len(data))
