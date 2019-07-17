cc96bf8eee746c562e997b44987151619b90d227
fdur <chantmforfun@gmail.com>
2019-07-17 13:09:29 +0800

    add filltering

--- read.py
+++ read.py
@@ -13,3 +13,15 @@ for d in data:
 	sum_len = sum_len + len(d)
 	
 print('字元的平均長度是', sum_len / len(data))
+
+#篩選清單
+new = []
+for d in data:
+	if len(d) < 100:
+		new. append(d)
+print('一共有', len(new), '長度小於100')
+print(new[0])
+
+print(len(new[0]))
+
+print('第一筆小於100的資料有', len(new[0]), '個字元')
